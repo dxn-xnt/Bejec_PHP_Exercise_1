4ddb0ff0e1cb0076dbf1ec58089ee04f0bfb778f
Dxn <157562104+dxn-xnt@users.noreply.github.com>
2025-02-12 13:29:49 +0800

    Update exercise_2.php

--- exercise_2.php
+++ exercise_2.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Exercise 1</title>
+    <title>PHP Exercise 2</title>
     <style>
         html,
         body {
