92511af0f4f488d60afa500e809fa98c5e553f66
Dxn <157562104+dxn-xnt@users.noreply.github.com>
2025-02-14 00:52:40 +0800

    Update exercise_2.php

--- exercise_2/exercise_2.php
+++ exercise_2/exercise_2.php
@@ -13,24 +13,38 @@
         <?php
         $cities = ['Tokyo', 'Mexico City', 'New York City', 'Mumbai', 'Seoul', 'Shanghai', 'Lagos', 'Buenos Aires', 'Cairo', 'London'];
 
-        echo "<div class=\"city-list\";><h1>Largest Cities</h1><br> <p>";
+        $sortedCities = $cities;
+        sort($sortedCities);
+
+        $updatedCities = $cities;
+        array_push($updatedCities, 'Los Angeles', 'Calcutta', 'Osaka', 'Beijing');
+
+        $sortedNewCities = $updatedCities;
+        sort($sortedNewCities);
+
+        echo "<div class=\"city-list\">
+                <h1>Largest Cities</h1><br> 
+                <p>";
         foreach ($cities as $city) echo "$city,  ";
-        echo "</p></div>";
+        echo "</p>
+            </div>
+            <div class=\"city-list\">
+                <h1>Sorted List</h1><br>
+                <ul>";
+        foreach ($sortedCities as $city) echo "<li>$city</li>";
+        echo "</ul>
+            </div>
+            <div>
+                <h3>Los Angeles, Calcutta, Osaka, Beijing added to the list!</h3>
+            </div>
 
-        sort($cities);
-        echo "<div class=\"city-list\";><h1>Sorted List</h1><br><ul>";
-        foreach ($cities as $city) echo "<li>$city</li>";
+            <div class=\"city-list\">
+                <h1>Sorted List with New Added Cities</h1><br> 
+                <ul>";
+        foreach ($sortedNewCities as $city) echo "<li>$city</li>";
         echo "</ul></div>";
-
-        array_push($cities, 'Los Angeles', 'Calcutta', 'Osaka', 'Beijing',);
-        echo "<div><h3>Los Angeles, Calcutta, Osaka, Beijing added to the list!</h3></div>";
-
-        sort($cities);
-        echo "<div class=\"city-list\";><h1>Sorted List with New Added Cities</h1><br> <ul>";
-        foreach ($cities as $city) echo "<li>$city</li>";
-        echo "</ul></div>";
-
         ?>
+
     </div>
 
 </body>
